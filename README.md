8df8ddf099f27f4f88e9577dbee9e40d09d81c4e
Grasseh <steve.gagnev4si@gmail.com>
2017-05-31 22:42:38 -0400

    Only commit lol

---
+++ README.md
@@ -0,0 +1,3 @@
+# Analyzing Monty Python
+
+This is a quick project to display that if the announcer wouldn't know behind which car is the door (and thus adding the possibility of you losing before getting to the moment you'd choose if you keep or change doors), you'd have 50-50 chances at winning compared to the 66-33 chances of the real game (where he knows where the car is).
---
+++ monty.py
@@ -0,0 +1,34 @@
+import random
+
+WinnersKeep = 0
+TotalEnd = 0
+TotalWithBadEnding = 0
+
+
+for i in range(1,10000+1):
+    CarDoor = random.randint(1,3) #Door that hides the prize
+    PlayerDoor = random.randint(1,3) #Door that the player chose as his door
+    TotalWithBadEnding += 1
+    #Select door to open
+    SameDoor = True
+    OpenedDoor = PlayerDoor
+    while(SameDoor):
+        OpenedDoor = random.randint(1,3)
+        SameDoor = OpenedDoor == PlayerDoor
+    #Only continue if we open a sheep
+    if(OpenedDoor != CarDoor):
+        #Let's assume the player always keeps his door at the end.
+        #If his door number is the same as the winning door, he wins
+        if(CarDoor == PlayerDoor):
+            WinnersKeep +=1
+        #Inc the amount of games that made it to the end no matter if he won or not
+        TotalEnd += 1
+
+print "Amount of situations where the game ended and the player won the car by keeping his door : "
+print WinnersKeep
+print "Total amount of games which made it to the end without revealing the car : "
+print TotalEnd
+print "Total amount of games : "
+print TotalWithBadEnding
+print "Percent of games that made it to the end that the player won by keeping his door : "
+print float(WinnersKeep) / TotalEnd * 100
